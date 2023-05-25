a43af788a9d871ba69773f0f8d7fabd76c2b0870
Alessandro Wako <114119907+AlexWako@users.noreply.github.com>
2023-05-25 16:04:26 +0900

    Add files via upload

---
+++ TextEdit.py
@@ -0,0 +1,13 @@
+def remove_space(string):
+    string = string.replace(' ','')
+    return string
+
+def list_to_html(data):
+    html_code = "<table width='100%'>\n"
+    for row in data:
+        html_code += "<tr>\n"
+        for cell in row:
+            html_code += f"<td>{cell}</td>\n"
+        html_code += "</tr>\n"
+    html_code += "</table>"
+    return html_code
